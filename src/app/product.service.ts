0b30c2664d43b3963def2d6e72c290d3a86e9768
Oscar Aksanti <aksantioscar@gmail.com>
2021-11-14 22:39:03 +0100

    Responding to 6th Q -

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -19,7 +19,7 @@ export class ProductService {
   }
 
   getProducts(): Observable<Product[]>{
-    return this._http.get(this._productUrl).map(response => <Product[]>response.json());
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 
 }
