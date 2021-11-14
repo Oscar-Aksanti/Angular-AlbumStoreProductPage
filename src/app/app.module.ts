fb082e566a0ce32b7db0051506a24bcc830149da
Oscar Aksanti <aksantioscar@gmail.com>
2021-11-14 22:05:15 +0100

    Responding to 6th question

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,14 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
     ProductDescriptionComponent, 
-    ProductService, ProductTracklistingComponent
+    ProductService, ProductTracklistingComponent, ProductListComponent
   ],
   imports: [
     BrowserModule,
